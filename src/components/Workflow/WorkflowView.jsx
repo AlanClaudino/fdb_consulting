d0a869b33a32c2b4383947be0883c995717ebadd
Alan Claudino <alanrodrigoclaudino@gmail.com>
2023-06-06 05:50:51 -0300

    workfloww UI updating

--- src/components/Workflow/WorkflowView.jsx
+++ src/components/Workflow/WorkflowView.jsx
@@ -1,13 +1,43 @@
-import React from 'react';
-import { WorflowContainer } from './styled';
-import { SubTitle, Title } from '../styled/styled';
+import {DarkButton, InnerSection, Section, WorflowContainer} from "./styled";
+import {Text, Title} from "../styled/styled";
+import {Edit2Icon, PlusIcon} from "lucide-react";
 
 const WorkflowView = () => {
   return (
     <WorflowContainer>
-      <Title style={{ textAlign: 'start' }}>Production Systems</Title>
+      <Title style={{textAlign: "start"}}>Production Systems</Title>
       <section>
-        <SubTitle style={{ textAlign: 'start' }}>Farm Crops</SubTitle>
+        <DarkButton>
+          <PlusIcon size={16} /> Create new Workflow
+        </DarkButton>
+        <Section style={{backgroundColor: "#0d2329", color: "#f4f4f4"}}>
+          <InnerSection>
+            <Text style={{fontWeight: "bolder"}}>Id</Text>
+          </InnerSection>
+          <InnerSection>
+            <Text style={{fontWeight: "bolder"}}>Crop</Text>
+          </InnerSection>
+          <InnerSection>
+            <Text style={{fontWeight: "bolder"}}>Description</Text>
+          </InnerSection>
+          <InnerSection>
+            <Text style={{fontWeight: "bolder"}}>Manage</Text>
+          </InnerSection>
+        </Section>
+        <Section>
+          <InnerSection>
+            <Text>001</Text>
+          </InnerSection>
+          <InnerSection>
+            <Text>Soybean</Text>
+          </InnerSection>
+          <InnerSection>
+            <Text>No Tillage</Text>
+          </InnerSection>
+          <InnerSection>
+            <Edit2Icon />
+          </InnerSection>
+        </Section>
       </section>
     </WorflowContainer>
   );
--- src/components/Workflow/styled.jsx
+++ src/components/Workflow/styled.jsx
@@ -1,10 +1,39 @@
-import styled from 'styled-components';
+import styled from "styled-components";
 
-export const WorflowContainer = styled('div')`
+export const WorflowContainer = styled("div")`
   display: flex;
   flex-direction: column;
   align-items: stretch;
   gap: 20px;
   width: 100%;
   height: 100%;
+`;
+
+export const DarkButton = styled("button")`
+  background-color: #0d2329;
+  display: flex;
+  align-items: center;
+  gap: 5px;
+  color: #fff;
+  border-radius: 4px;
+  font-size: 16px;
+  padding: 12px;
+  border: none;
+  &:hover {
+    background-color: #4dd467;
+    cursor: pointer;
+  }
+`;
+
+export const Section = styled("section")`
+  padding: 10px;
+  display: flex;
+  flex-wrap: wrap;
+  align-items: center;
+  justify-content: start;
+  border-radius: 5px;
+`;
+
+export const InnerSection = styled.div`
+  flex-grow: 1;
 `;
