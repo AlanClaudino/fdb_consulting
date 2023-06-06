6645e62724fa98cfa63491b67c791a7137b969f3
AlanClaudino <alanrodrigoclaudino@gmail.com>
2023-06-06 08:14:07 -0300

    Updating workflow UI

--- src/components/Workflow/WorkflowView.jsx
+++ src/components/Workflow/WorkflowView.jsx
@@ -1,30 +1,41 @@
-import {DarkButton, InnerSection, Section, WorflowContainer} from "./styled";
-import {Text, Title} from "../styled/styled";
-import {Edit2Icon, PlusIcon} from "lucide-react";
+import {
+  DarkButton,
+  DeleteButton,
+  EditButton,
+  InnerSection,
+  TableContainer,
+  TableData,
+  TableTitle,
+  WorflowContainer,
+} from './styled';
+import { Text, Title } from '../styled/styled';
+import { DeleteIcon, Edit2Icon, PlusIcon, Trash, Trash2 } from 'lucide-react';
 
 const WorkflowView = () => {
   return (
     <WorflowContainer>
-      <Title style={{textAlign: "start"}}>Production Systems</Title>
+      <Title style={{ textAlign: 'start' }}>Production Systems</Title>
       <section>
         <DarkButton>
           <PlusIcon size={16} /> Create new Workflow
         </DarkButton>
-        <Section style={{backgroundColor: "#0d2329", color: "#f4f4f4"}}>
+      </section>
+      <TableContainer>
+        <TableTitle>
           <InnerSection>
-            <Text style={{fontWeight: "bolder"}}>Id</Text>
+            <Text>Id</Text>
           </InnerSection>
           <InnerSection>
-            <Text style={{fontWeight: "bolder"}}>Crop</Text>
+            <Text>Crop</Text>
           </InnerSection>
           <InnerSection>
-            <Text style={{fontWeight: "bolder"}}>Description</Text>
+            <Text>Description</Text>
           </InnerSection>
           <InnerSection>
-            <Text style={{fontWeight: "bolder"}}>Manage</Text>
+            <Text>Manage</Text>
           </InnerSection>
-        </Section>
-        <Section>
+        </TableTitle>
+        <TableData>
           <InnerSection>
             <Text>001</Text>
           </InnerSection>
@@ -35,10 +46,34 @@ const WorkflowView = () => {
             <Text>No Tillage</Text>
           </InnerSection>
           <InnerSection>
-            <Edit2Icon />
+            <EditButton>
+              <Edit2Icon size={18} />
+            </EditButton>
+            <DeleteButton>
+              <Trash2 size={18} />
+            </DeleteButton>
           </InnerSection>
-        </Section>
-      </section>
+        </TableData>
+        <TableData>
+          <InnerSection>
+            <Text>001</Text>
+          </InnerSection>
+          <InnerSection>
+            <Text>Soybean</Text>
+          </InnerSection>
+          <InnerSection>
+            <Text>No Tillage</Text>
+          </InnerSection>
+          <InnerSection>
+            <EditButton>
+              <Edit2Icon size={18} />
+            </EditButton>
+            <DeleteButton>
+              <Trash2 size={18} />
+            </DeleteButton>
+          </InnerSection>
+        </TableData>
+      </TableContainer>
     </WorflowContainer>
   );
 };
--- src/components/Workflow/styled.jsx
+++ src/components/Workflow/styled.jsx
@@ -1,6 +1,6 @@
-import styled from "styled-components";
+import styled from 'styled-components';
 
-export const WorflowContainer = styled("div")`
+export const WorflowContainer = styled('div')`
   display: flex;
   flex-direction: column;
   align-items: stretch;
@@ -9,7 +9,7 @@ export const WorflowContainer = styled("div")`
   height: 100%;
 `;
 
-export const DarkButton = styled("button")`
+export const DarkButton = styled('button')`
   background-color: #0d2329;
   display: flex;
   align-items: center;
@@ -25,15 +25,53 @@ export const DarkButton = styled("button")`
   }
 `;
 
-export const Section = styled("section")`
+export const TableContainer = styled.section`
+  border-radius: 5px;
+  border: 1px solid #0d2329;
+`;
+
+export const TableTitle = styled.section`
+  padding: 10px;
+  display: flex;
+  align-items: center;
+  background-color: #0d2329;
+  color: #f4f4f4;
+  font-weight: bolder;
+`;
+
+export const TableData = styled('section')`
   padding: 10px;
   display: flex;
   flex-wrap: wrap;
   align-items: center;
-  justify-content: start;
-  border-radius: 5px;
+  border-top: 1px solid #0d2329;
 `;
 
 export const InnerSection = styled.div`
-  flex-grow: 1;
+  flex: 1 1 0px;
+  display: flex;
+  align-items: center;
+  gap: 15px;
+`;
+
+export const EditButton = styled.button`
+  display: flex;
+  align-items: center;
+  background-color: #fafafa;
+  border: none;
+  &:hover {
+    cursor: pointer;
+    color: #00cc66;
+  }
+`;
+
+export const DeleteButton = styled.button`
+  display: flex;
+  align-items: center;
+  background-color: #fafafa;
+  border: none;
+  &:hover {
+    cursor: pointer;
+    color: #db3a34;
+  }
 `;
